c79eecb8e8e432ac1b1bb51ad7ce77b56f0f967c
Zetta <zeyangbai.rvo@gmail.com>
2023-11-07 00:37:55 +0800

    dl_homework

--- Getting Started/simpleMLP.py
+++ Getting Started/simpleMLP.py
@@ -59,6 +59,7 @@ def binary_cross_entropy_derivative(y_true: np.ndarray, y_pred: np.ndarray) -> n
     """
     return -(y_true / y_pred) + (1 - y_true) / (1 - y_pred)
 
+
 class MLP:
     def __init__(self, input_size: int, hidden_sizes: Tuple[int, int], output_size: int):
         # 初始化权重和偏置, 从标准正态分布中返回一个或多个样本值
@@ -87,36 +88,35 @@ class MLP:
         return p
 
     def backward(self, x: np.ndarray, y: np.ndarray, output: np.ndarray):
-        # 反向传播
+        # 反向传播, 采用随机梯度下降算法
         e = binary_cross_entropy(y, output)  # e = -ylog(p)-(1-y)log(1-p)
         w3_delta = binary_cross_entropy_derivative(y, output) * sigmoid_derivative(output) * self.w3  # w3'=e'p'y'
-        output_error = binary_cross_entropy(y, output)
-        output_delta = output_error * sigmoid_derivative(output)  # p'=y(1-y)y'=sigmoid_derivative(output)*w
+        w2_delta = w3_delta.dot(self.w3.T) * tanh_derivative(self.h2) * self.w2  # w2'=w3'w3tanh'(h2)w2
+        w1_delta = w2_delta.dot(self.w2.T) * tanh_derivative(self.h1) * self.w1  # w1'=w2'w2tanh'(h1)w1
+        b3_delta = binary_cross_entropy_derivative(y, output) * sigmoid_derivative(output) * self.b3  # b3'=e'p'y'
+        b2_delta = w3_delta.dot(self.w3.T) * tanh_derivative(self.h2) * self.b2  # b2'=w3'w3tanh'(h2)b2
+        b1_delta = w2_delta.dot(self.w2.T) * tanh_derivative(self.h1) * self.b1  # b1'=w2'w2tanh'(h1)b1
+        parameters = [w3_delta, w2_delta, w1_delta, b3_delta, b2_delta, b1_delta]
+        return parameters
 
-        hidden2_error = output_delta.dot(self.weights_hidden2_to_output.T)
-        hidden2_delta = hidden2_error * sigmoid_derivative(self.hidden2)
-
-        hidden1_error = hidden2_delta.dot(self.weights_hidden1_to_hidden2.T)
-        hidden1_delta = hidden1_error * sigmoid_derivative(self.hidden1)
-
-        return ...
-
+    def optimize(self, parameters: list, learning_rate: float):
         # 更新权重和偏置
-        self.weights_hidden2_to_output += self.hidden2.T.dot(output_delta)
-        self.bias_output += np.sum(output_delta, axis=0)
-
-        self.weights_hidden1_to_hidden2 += self.hidden1.T.dot(hidden2_delta)
-        self.bias_hidden2 += np.sum(hidden2_delta, axis=0)
-
-        self.weights_input_to_hidden1 += x.T.dot(hidden1_delta)
-        self.bias_hidden1 += np.sum(hidden1_delta, axis=0)
+        self.w3 -= learning_rate * parameters[0]
+        self.w2 -= learning_rate * parameters[1]
+        self.w1 -= learning_rate * parameters[2]
+        self.b3 -= learning_rate * parameters[3]
+        self.b2 -= learning_rate * parameters[4]
+        self.b1 -= learning_rate * parameters[5]
 
     def train(self, x: np.ndarray, y: np.ndarray, epochs: int, learning_rate: float):
-        for epoch in range(epochs):
+        for epoch in range(1, epochs + 1):
+            # 正向传播
             output = self.forward(x)
-            self.backward(x, y, output)
-            loss = binary_cross_entropy(y, output)
-            print(f'Epoch {epoch + 1}/{epochs} loss: {loss:.4f}')
+            # 反向传播
+            parameters = self.backward(x, y, output)
+            # 更新权重和偏置
+            self.optimize(parameters, learning_rate)
+            print(f'Epoch: {epoch}, Loss: {binary_cross_entropy(y, output):.2f}')
 
     def predict(self, x: np.ndarray) -> np.ndarray:
         return self.forward(x)
@@ -125,3 +125,26 @@ class MLP:
 # mlp = MLP(input_size=4, hidden_sizes=(10, 5), output_size=1)
 # mlp.train(X_train, y_train, epochs=1000, learning_rate=0.1)
 # predictions = mlp.predict(X_test)
+import sklearn
+
+# 导入iris
+iris = sklearn.datasets.load_iris()
+X = iris.data
+y = iris.target
+# 将y转换为one-hot编码
+y_one_hot = np.zeros((y.shape[0], 3))
+y_one_hot[np.arange(y.shape[0]), y] = 1
+# 划分训练集和测试集
+from sklearn.model_selection import train_test_split
+train_set = train_test_split(X, y_one_hot, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_set
+# 创建MLP
+mlp = MLP(input_size=4, hidden_sizes=(10, 5), output_size=3)
+# 训练
+mlp.train(X_train, y_train, epochs=1000, learning_rate=0.1)
+# 预测
+predictions = mlp.predict(X_test)
+# 评估
+from sklearn.metrics import accuracy_score
+print(f'Accuracy: {accuracy_score(np.argmax(y_test, axis=1), np.argmax(predictions, axis=1)):.2f}')
+
--- Getting Started/test.py
+++
@@ -1,9 +0,0 @@
-import random
-import torch
-import torch.nn.functional as F
-
-X = torch.arange(0, 10)
-X.reshape(2, 5)
-
-X = F.one_hot(X.T, 28)
-print(X)
