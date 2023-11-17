fbb1abfb9c619106d81533eb6a2a1c79c9c1e61e
Rvosuke <zeyangbai.rvo@gmail.com>
2023-11-17 17:21:04 +0800

    17-11-2023

--- MNIST/resnet4mnist/resnet.py
+++ MNIST/resnet4mnist/resnet.py
@@ -4,9 +4,10 @@ import logging
 import matplotlib.pyplot as plt
 import torch.nn.functional as F
 from torch import nn, optim
+from torch.optim.lr_scheduler import StepLR
 from datetime import datetime
 from MNIST.utils.view import plot_history
-from MNIST.utils.mnist_dataload import create_dataset
+from MNIST.utils.mnist_dataload import mnist_dataset
 from MNIST.utils.device_set import device_setting
 from MNIST.utils.log import setup_logger
 
@@ -31,61 +32,59 @@ class ResBlock(nn.Module):
 
 
 class ResNet(nn.Module):
-    def __init__(self, n_chans1=32, n_blocks=100):
+    def __init__(self, n_chans1=32, n_blocks=16):
         super().__init__()
         self.n_chans1 = n_chans1  # 通道数
         self.conv1 = nn.Conv2d(1, n_chans1, kernel_size=3, padding=1)
         self.resblocks = nn.Sequential(*(n_blocks * [ResBlock(n_chans=n_chans1)]))
-        self.fc1 = nn.Linear(8 * 8 * n_chans1, 32)
-        self.fc2 = nn.Linear(32, 10)
+        self.fc1 = nn.Linear(7 * 7 * n_chans1, 64)
+        self.fc2 = nn.Linear(64, 10)
 
     def forward(self, x):
         out = F.max_pool2d(torch.relu(self.conv1(x)), 2)  # nc*28*28 -> nc*14*14
         out = self.resblocks(out)  # nc*14*14 -> nc*14*14
         out = F.max_pool2d(out, 2)  # nc*14*14 -> nc*7*7
-        out = out.view(-1, 8 * 8 * self.n_chans1)
+        out = out.view(x.shape[0], -1)
         out = torch.relu(self.fc1(out))
         out = self.fc2(out)
         return out
 
 
-def train(data_dir, lr=0.01, num_epochs=20):
-    ds_train = create_dataset(data_dir, training=True)
-    ds_eval = create_dataset(data_dir, training=False)
-    device = device_setting(num_cores=20)
-    net = ResNet().to(device)
-    loss = nn.CrossEntropyLoss()
+def train(batch_size=128, lr=0.001, num_epochs=60):
+    ds_train, ds_eval = mnist_dataset(batch_size=batch_size)
+    device = device_setting()
+    net = ResNet()
+    loss_fn = nn.CrossEntropyLoss()
     opt = optim.Adam(net.parameters(), lr=lr)
+    net.to(device)
 
     train_losses = []
     val_accuracies = []
 
-    for epoch in range(1, 1+num_epochs):
+    for epoch in range(1, 1 + num_epochs):
         running_loss = 0.0
-
-        for i, data in enumerate(ds_train, 0):
-            inputs, labels = data
+        batch_num = 1
+        for i, (inputs, labels) in enumerate(ds_train, 0):
             inputs, labels = inputs.to(device), labels.to(device)  # 将输入和标签移动到所选设备
             opt.zero_grad()
             outputs = net(inputs)
-            loss_output = loss(outputs, labels)
-            loss_output.backward()
+            loss = loss_fn(outputs, labels)
+            loss.backward()
             opt.step()
-
-            running_loss += loss_output.item()
-
+            running_loss += loss.item()
+            batch_num = i
+        batch_num += 1
+        running_loss /= batch_num
         train_losses.append(running_loss)
-        logging.info(f'Epoch {epoch + 1}, Loss: {running_loss:.4f}')
+        logging.info(f'Epoch {epoch}, Loss: {running_loss:.4f}')
 
-        # 每个epoch结束后，计算在验证集上的准确率
         correct = 0
         total = 0
         with torch.no_grad():
-            for data in ds_eval:
-                images, labels = data
+            for images, labels in ds_eval:
                 images, labels = images.to(device), labels.to(device)
                 outputs = net(images)
-                _, predicted = torch.max(outputs.data, 1)
+                predicted = torch.argmax(outputs.data, 1)
                 total += labels.size(0)
                 correct += (predicted == labels).sum().item()
         acc = 100 * correct / total
@@ -99,9 +98,10 @@ def main():
 
     # 定义超参数
     hyperparameters = {
-        'data_dir': 'MNIST/MNIST_Data',
-        'lr': 3e-5,
-        'num_epochs': 100,
+        'batch_size': 256,
+        'lr': 5e-5,
+        'num_epochs': 40,
+        'model': 'ResNet16'
     }
 
     # 记录训练的开始时间
@@ -109,7 +109,7 @@ def main():
 
     # 训练模型
     net, train_losses, val_acc = (
-        train(hyperparameters['data_dir'], hyperparameters['lr'], hyperparameters['num_epochs']))
+        train(hyperparameters['batch_size'], hyperparameters['lr'], hyperparameters['num_epochs']))
 
     # 绘制训练历史记录
     title = f"ResNet_{timestamp}(lr={hyperparameters['lr']}, epochs={hyperparameters['num_epochs']})"
@@ -120,7 +120,7 @@ def main():
         json.dump(hyperparameters, f, indent=4)
 
     # 保存模型参数
-    torch.save(net.state_dict(), f'resnet_{timestamp}.pth')
+    torch.save(net.state_dict(), f'resnet_{timestamp}_emnist.pth')
 
 
 if __name__ == '__main__':
--- MNIST/utils/mnist_dataload.py
+++ MNIST/utils/mnist_dataload.py
@@ -2,17 +2,31 @@ from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 
 
-def create_dataset(data_dir, training=True, batch_size=64,
-                   resize=(32, 32), rescale=1 / (255 * 0.3081), shift=-0.1307 / 0.3081):
+def mnist_dataset(batch_size=128):
     # 这里的shift和rescale是根据MNIST数据集的均值和标准差提前计算得到的
     # 数据处理：数据集加载、缩放、归一化、格式转换、洗牌、批标准化。
+    rescale = 1 / (255 * 0.3081)
+    shift = -0.1307 / 0.3081
     transform = transforms.Compose([
-        transforms.Resize(resize),
         transforms.ToTensor(),
         transforms.Normalize(mean=[shift], std=[rescale])
     ])
 
-    dataset = datasets.MNIST(root=data_dir, train=training, transform=transform, download=True)
-    loader = DataLoader(dataset, batch_size=batch_size, shuffle=training)
+    dataset = datasets.MNIST(root="../MNIST_Data", train=True, transform=transform, download=True)
+    test = datasets.MNIST(root="../MNIST_Data", train=False, transform=transform, download=True)
+    loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test, batch_size=batch_size*2)
 
-    return loader
+    return loader, test_loader
+
+
+def emnist_dataset(batch_size=64, transform=transforms.ToTensor()):
+    t = transforms.Compose([
+        transform
+    ])
+
+    train_dataset = datasets.EMNIST(root="../MNIST_Data", split='letters', train=True, transform=t, download=True)
+    test_dataset = datasets.EMNIST(root="../MNIST_Data", split='letters', train=False, transform=t, download=True)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
+    return train_loader, test_loader
