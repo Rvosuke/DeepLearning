2961b46636cee26917d6162a687c57a2d799a212
Zetta <zeyangbai.rvo@gmail.com>
2023-12-09 16:02:55 +0800

    set position embedding settable

--- gnn/datas.py
+++ gnn/datas.py
@@ -13,22 +13,20 @@ def read_data(file_path, is_adjacency=False):
     # read data
     data = np.loadtxt(file_path, delimiter=",", skiprows=1, usecols=np.arange(1, num_cols + 1))
     
-    # if the data is an adjacency matrix, convert it to a networkx object
-    if is_adjacency:
-        return nx.from_numpy_array(data, parallel_edges=False, create_using=None)
     return data
 
 
-def split(expression, target, graph, encode_dim=3):
+def split(expression, target, graph, encode_dim):
     train_list, test_list, valid_list = [], [], []
     num_sample = len(target)
     train_index, val_index = int(num_sample*0.8), int(num_sample*0.9)
     positional_encoder = torch.rand(31, encode_dim).float()
 
     for i in range(num_sample):
-        x_yeet = expression[i]
-        x_scalar = torch.unsqueeze(x_yeet, dim=1).float()
-        x = torch.cat((x_scalar, positional_encoder), 1)
+        if encode_dim > 0:
+            x_yeet = expression[i]
+            x_scalar = torch.unsqueeze(x_yeet, dim=1).float()
+            x = torch.cat((x_scalar, positional_encoder), 1)
         y = target[i]
 
         params = {"x": x, "y": y, "edge_index": graph.edge_index, "edge_attr": graph.weight}
@@ -42,18 +40,3 @@ def split(expression, target, graph, encode_dim=3):
 
     return train_list, valid_list, test_list
 
-
-adj_matrix = read_data("adjacency_matrix.csv", is_adjacency=True)
-expression_matrix = read_data("expression.csv")
-binary_diagnosis = read_data("target.csv")
-
-# prevent target feature from leaking information and construct virtual nodes
-in_edge = adj_matrix[-1].astype(bool)  # the last row of the adjacency matrix is the virtual node
-for sample in expression_matrix:
-    sample[-1] = np.mean(sample[in_edge])
-
-x_tensor = torch.from_numpy(expression_matrix).float()
-diagnosis_tensor = torch.Tensor(binary_diagnosis).long()
-G_convert = torch_geometric.utils.from_networkx(G)
-
-data_split = split(x_tensor, diagnosis_tensor, G_convert)
--- gnn/main.py
+++ gnn/main.py
@@ -2,8 +2,9 @@ import warnings
 import torch
 
 from torch_geometric.loader import DataLoader
+from torch_geometric.utils import from_networkx
 
-from datas import data_split
+from datas import split, read_data
 from models import GCNGraph
 from utils import train, evaluate
 
@@ -11,23 +12,39 @@ warnings.filterwarnings('ignore')
 
 
 def main():
+    adj, expression, diagnosis = read_data("adjacency_matrix.csv", is_adjacency=True), read_data("expression.csv"), read_data("target.csv")
+    
+    # prevent target feature from leaking information and construct virtual nodes
+    in_edge = adj[-1].astype(bool)  # the last row of the adjacency matrix is the virtual node
+    for sample in expression:
+        sample[-1] = np.mean(sample[in_edge])
+    G = nx.from_numpy_array(adj, parallel_edges=False, create_using=None)
+
+    x_tensor = torch.from_numpy(expression).float()
+    diagnosis_tensor = torch.Tensor(diagnosis).long()
+    G_convert = from_networkx(G)
+
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     args = {
         'batch_size': 32,
-        'gcn_params': {
-            'in_channels': 4,
-            'hidden_dim': 256,
-            'out_channels': 1,
-            'gcn_layerss': 5,
-            'gcn_base_layers': 3,
-            'dropout': 0.3,
-        }
+        'no_pos': True,  # if False, positional encoding will not be used
         'lr': 1e-3,
         'epochs': 100,
     }
-    train_loader = DataLoader(data_split[0], batch_size=args['batch_size'], shuffle=True)
-    valid_loader, test_loader = DataLoader(data_split[1]), DataLoader(data_split[2])
-    model = GCNGraph(**args[gcn_params]).to(device)
+    gcn_params = {
+        'in_channels': 1 if args['no_pos'] else 4,
+        'hidden_dim': 256,
+        'out_channels': 1,
+        'gcn_layers': 5,
+        'gcn_base_layers': 3,
+        'dropout': 0.3,
+    }
+    encode_dim = 0 if args['no_pos'] else gcn_params['in_channels']
+
+    train, val, test = split(x_tensor, diagnosis_tensor, G_convert, encode_dim)
+    train_loader = DataLoader(train, batch_size=args['batch_size'], shuffle=True)
+    valid_loader, test_loader = DataLoader(val), DataLoader(test)
+    model = GCNGraph(**gcn_params).to(device)
     model.reset_parameters()
     optimizer = torch.optim.Adam(model.parameters(), lr=args['lr'])
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.3)
